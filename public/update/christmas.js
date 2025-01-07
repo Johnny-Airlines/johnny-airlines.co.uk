32c241feb69dbc8c2434529a305a9f8348c05098
John Bass <CEO@johnny-airlines.co.uk>
2025-01-07 19:15:18 +0000

    Smallest ever optimization broke the edge of the map, now reverted

--- public/update/christmas.js
+++ public/update/christmas.js
@@ -43,7 +43,7 @@ var gameArea = {
     canvas: document.createElement("canvas"),
     start: function () {
         gameArea.resize();
-        this.context = this.canvas.getContext("2d",{alpha:false});
+        this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     },
     resize: function () {
