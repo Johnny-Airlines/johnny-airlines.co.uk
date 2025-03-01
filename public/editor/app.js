8d9eb4301284087e8fee3f4d2d3aba15e6f0fec3
John Bass <CEO@johnny-airlines.co.uk>
2025-03-01 22:34:18 +0000

    More work on editor

--- public/editor/app.js
+++ public/editor/app.js
@@ -1,11 +1,55 @@
-const value = /* set from `myEditor.getModel()`: */ `function hello() {
-	alert('Hello world!');
-}`;
-
-// Hover on each property to see its docs!
-const myEditor = monaco.editor.create(document.getElementById("container"), {
-	value,
-	language: "javascript",
-	automaticLayout: true,
+var editor = monaco.editor.create(document.getElementById('container'), {
+	value: ['def x():', '\tprint("hey")', ''].join('\n'),
+	language: 'python'
 });
+async function sendData(data) {
+	// Construct a FormData instance
+	const formData = new FormData();
 
+	// Add a text field
+	//formData.append("name", "Pomegranate");
+
+	// Add a file
+	/*
+	const selection = await window.showOpenFilePicker();
+	if (selection.length > 0) {
+		const file = await selection[0].getFile();
+		formData.append("file", file);
+	}*/
+
+	const file = new File(editor.getValue().split(""), document.getElementById("filenameInput").value);
+	formData.append("file",file);
+
+	try {
+		const response = await fetch("http://api.johnny-airlines.co.uk:5000/uploadFile", {
+			method: "POST",
+			// Set the FormData instance as the request body
+			body: formData,
+			headers: {
+				'Access-Control-Allow-Origin':'*'
+			}
+		});
+		console.log(await response.json());
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+async function runFile() {
+	const url = `http://api.johnny-airlines.co.uk:5000/runPyFile/${document.getElementById("filenameInput").value}/a`
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`)
+		}
+
+		const json = await response;
+		document.getElementById("output").value = json
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+
+document.getElementById("sendFileBtn").addEventListener("click", sendData);
+document.getElementById("runCodeBtn").addEventListener("click", runFile);
