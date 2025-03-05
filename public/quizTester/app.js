33a743c1ff49f367fab690458af2a3191cbcbdc2
John Bass <CEO@johnny-airlines.co.uk>
2025-03-05 22:24:07 +0000

    Make multiple choice

--- public/quizTester/app.js
+++ public/quizTester/app.js
@@ -2,15 +2,24 @@ var quizData;
 getData().then(resp => quizData=resp).catch(e=>console.log(e));
 
 var quizNumber = 0;
+var correctNumber = 0;
 
 function setQuestion() {
 	quizNumber += 1;
 	quizObject = quizData.results[quizNumber]
+	console.log(quizObject)
 	document.getElementById("currentQuestion").innerText = quizObject.question;
+	correctNumber = Math.floor(Math.random()*4)
+	answers = quizObject.incorrect_answers;
+	answers = answers.splice(correctNumber, 0, quizObject.correct_answer)
+	console.log(answers)
+	for (let i = 0; i < 4; i++) {
+		document.getElementById(`label${i+1}`).innerText = quizObject.incorrect_answers[i]
+	}
 }
 
 function checkQuestion() {
-	if (document.getElementById("answer").value == quizData.results[quizNumber].correct_answer) {
+	if (document.getElementById(`${correctNumber+1}`).checked == true) {
 		alert("correct")
 	} else {
 		alert(quizData.results[quizNumber].correct_answer)
