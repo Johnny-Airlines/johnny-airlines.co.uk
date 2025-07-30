2293fd2ec07a8ff917a3754c0cc4a0d055cfe9b2
John Bass <CEO@johnny-airlines.co.uk>
2025-07-30 15:38:11 +0100

    oops, small hotfix

--- public/update/christmas.js
+++ public/update/christmas.js
@@ -1,4 +1,4 @@
-//(function(){ "use strict"
+(function(){ "use strict"
 const firebaseConfig = {
 	apiKey: "AIzaSyDJlncorTA9lATy5t-1bH0OH-lK509ipFw",
 	authDomain: "johnnyairlinescouk.firebaseapp.com",
@@ -1988,4 +1988,4 @@ function updateGameArea(lastTimestamp) {
 		updateGameArea(currentTime)
 	}
 }
-//})();
+})();
