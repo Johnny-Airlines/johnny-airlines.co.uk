21635c119413d8c52de85b4ba912618e988f1c30
John Bass <CEO@johnny-airlines.co.uk>
2025-11-11 20:12:51 +0000

    Removing caching for html and css, rework 404, experiment with pyodide

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: 'public/',
 	globPatterns: [
-		'**/*.{html,txt,css,png,jpg,svg,ico,gif,json,mp3,md,webmanifest,ttf,otf}'
+		'**/*.{txt,png,jpg,svg,ico,gif,json,mp3,md,webmanifest,ttf,otf}'
 	],
 	swDest: 'public/sw.js',
 	ignoreURLParametersMatching: [
