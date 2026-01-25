5ede5fc68f1f6fd2b39475f9531b2e9dbc42e6dd
John Bass <CEO@johnny-airlines.co.uk>
2026-01-25 17:31:13 +0000

    Basic particle collisions

--- public/phySim/app.js
+++ public/phySim/app.js
@@ -1,5 +1,27 @@
 "use strict";
 
+function dot(a, b){
+	return a[0]*b[0]+a[1]*b[1];
+}
+
+function add(a, b) {
+	//console.log(`Add: ${a} + ${b} = ${[a[0]+b[0],a[1]+b[1]]}`);
+	return [a[0]+b[0],a[1]+b[1]];
+}
+
+function multiply(a, b) {
+	//console.log(`Multiply: ${a} * ${b} = ${[a[0]*b,a[1]*b]}`);
+	return [a[0]*b,a[1]*b];
+}
+
+function minus(a, b) {
+	return add(a,multiply(b,-1));
+}
+
+function magnitude(a) {
+	return (a[0]**2+a[1]**2)**0.5;
+}
+
 function ranInt(max,min) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -13,11 +35,13 @@ class Particle {
 		this.ax = 0;
 		this.ay = GRAVITY;
 		this.radius = 10;
+		this.mass = this.radius ** 2;
 		this.colour = `rgb(${ranInt(0,100)},${ranInt(0,100)},${ranInt(0,100)})`;
 		this.COLOUR = this.colour;
 	}
 	
 	update() {
+		this.previousState = [this.x,this.y];
 		this.ay = GRAVITY;
 		this.vx += this.ax;
 		this.vy += this.ay;
@@ -53,6 +77,20 @@ class Particle {
 						particle.colour = particle.COLOUR;
 						this.colour = this.COLOUR;
 					},5);
+					let m1 = this.mass;
+					let m2 = particle.mass;
+					let x1 = [this.x, this.y];
+					let x2 = [particle.x, particle.y];
+					let v1 = [this.vx, this.vy];
+					let v2 = [particle.vx, particle.vy];
+
+					let v1After = minus(v1,multiply(minus(x1,x2),(2*m2/(m1+m2))*dot(minus(v1,v2),minus(x1,x2))/magnitude(minus(x1,x2))**2));
+					let v2After = minus(v2,multiply(minus(x2,x1),(2*m1/(m1+m2))*dot(minus(v2,v1),minus(x2,x1))/magnitude(minus(x2,x1))**2));
+
+					this.vx = v1After[0];
+					this.vy = v1After[1];
+					particle.vx = v2After[0];
+					particle.vy = v2After[1];
 				}
 			}
 		});
@@ -105,13 +143,13 @@ ctx.translate(0, canvas.height);
 ctx.scale(1, -1);
 
 var particles = [];
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 50; i++) {
 	particles.push(new Particle());
 }
 
 update();
 function update() {
-	ctx.fillStyle = "rgba(255,255,255,0.1)";
+	ctx.fillStyle = "rgba(255,255,255,0.3)";
 	//ctx.fillStyle = "white";
 	ctx.fillRect(0,0,canvas.width,canvas.height);
 	particles.forEach((particle) => {
