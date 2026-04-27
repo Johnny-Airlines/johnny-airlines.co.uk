d3b84b2b7285534fed2d8d7641bfbb72cb92bcaa
Johnny-Airlines <CEO@johnny-airlines.co.uk>
2026-04-27 16:51:05 +0000

    More quiz progress

--- public/quiz/app.js
+++ public/quiz/app.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+
 firebase.auth().onAuthStateChanged((user) => {
 	if (user) {
 		// User is signed in, see docs for a list of available properties
@@ -19,13 +20,13 @@ firebase.auth().onAuthStateChanged((user) => {
 		const email =
 			user.email.replace("@johnny-airlines.co.uk", "") || prompt("Username: ");
 		const photoURL = user.photoURL;
-        alert("Signed in as " + displayName + " (" + email + ")");
-        loadQuiz();
+		loadQuiz();
 	} else {
 		//Redirect user to sign in page if not signed in
 		window.location.href = "./accounts.html";
 	}
 });
+
 
 async function loadJSON() {
     const response = await fetch("./questions.json");
@@ -33,18 +34,31 @@ async function loadJSON() {
     return data;
 }
 
+var questions;
+var questionSet;
 var question;
 var results = document.getElementById("results");
-console.log(results)
+var isBonus = false;
+var bonusIndex = 0;
+var remainingOptions = [];
+
 function loadQuiz() {
     loadJSON().then((data) => {
         questions = data;
-        nextQuestion();
+		document.getElementById("start").disabled = false;
+		document.getElementById("start").innerText = "Start";
     });
 }
+
 function nextQuestion() {
-    question = questions[Math.floor(Math.random() * questions.length)];
-	question = questions[9];
+	if (!isBonus) {
+		questionSet = questions[Math.floor(Math.random() * questions.length)];
+		questionSet = questions[4];
+		question = questionSet;
+	} else {
+		question = questionSet.Bonuses[bonusIndex];
+		bonusIndex++;
+	}
 	let delay = 0;
 	for (const word of question.Question.split(" ")) {
 		setTimeout(()=>{
@@ -52,14 +66,27 @@ function nextQuestion() {
 		},delay);
 		delay += 1000;
 	}
-}
-
-function bonus() {
-	
+	setTimeout(()=>{
+		document.getElementById("question").innerText = "";
+	},delay);
 }
 
 function buzz() {
     document.getElementById("answerDialog").showModal();
+	let id = setTimeout(function() {}, 0);
+	while (id--) {
+		clearTimeout(id);
+	}
+	document.getElementById("question").innerText = "You Buzzed!";
+	if (typeof question.Type == "number") {
+		document.getElementById("help").innerText = "Since you need to submit more than one answer hit submit each answer individually.";
+	}
+}
+
+function start() {
+	document.getElementById("container").showModal();
+	document.getElementById("start").style.display = "none";
+	nextQuestion();
 }
 
 function submit() {
@@ -69,11 +96,13 @@ function submit() {
         document.getElementById("resultsDialog").close();
     },1000);
 	const answer = document.getElementById("answer").value;
+	document.getElementById("answer").value = "";
 	if (question.Type == "Regular") {
 		if (answer.toLowerCase() === question.Answer.toLowerCase()) {
 			results.innerText = "Correct!";
 			setTimeout(() => {
-				bonus();
+				isBonus = true;
+				nextQuestion();
 			},1000);
 			return;
 		} else {
@@ -94,11 +123,30 @@ function submit() {
 				return;
 			}
 		}
-		results.innerText = "Incorrect! The acceptable answers were: "
+		results.innerText = "Incorrect! Any of the following are acceptable: "
 		for (const option of question.Answer) {
 			results.innerText += `${option}, `
 		}
-		results.innerText = results.innerText.substr(0,results.innerText.length-1)
+		results.innerText = results.innerText.substring(0,results.innerText.length)
+		return;
+	}
+	if (typeof question.Type == "number") {
+		for (const option of question.Answer) {
+			if (option.toLowerCase() == answer.toLowerCase()) {
+				results.innerText = "Correct!";
+
+				setTimeout(() => {
+					document.getElementById("resultsDialog").close();
+					document.getElementById("answerDialog").showModal();
+				},1000);
+				return;
+			}
+		}
+		results.innerText = "Incorrect! Any of the following are acceptable: "
+		for (const option of question.Answer) {
+			results.innerText += `${option}, `
+		}
+		results.innerText = results.innerText.substring(0,results.innerText.length)
 		return;
 	}
 }
