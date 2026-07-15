78aa3b69555e5603e1875f7b155cdebf0753e794
John Bass <CEO@johnny-airlines.co.uk>
2026-07-15 18:28:10 +0100

    Fix panning and cells manipulation to feel better and give more control

--- public/life/app.js
+++ public/life/app.js
@@ -61,29 +61,67 @@ var offsetX = 5;
 var offsetY = 5;
 
 var isMouseDown = false;
-var didDrag = false;
-canvas.addEventListener("mousedown", (e) => {isMouseDown = true;});
-canvas.addEventListener("mouseup", (e) => {
-	isMouseDown = false;
-	if (!didDrag) {
+var isDragKeyDown = false;
+const dragKey = "Control";
+var isDeleteKeyDown = false;
+var deleteKey = "Shift";
+addEventListener("keydown", (e) => {
+	if (e.key == dragKey) {
+		isDragKeyDown = true;
+		canvas.style.cursor = "grab"
+	}
+	if (e.key == deleteKey) {
+		isDeleteKeyDown = true;
+		canvas.style.cursor = "not-allowed";
+	}
+});
+addEventListener("keyup", (e) => {
+	if (isDragKeyDown && e.key == dragKey) {
+		isDragKeyDown = false;
+		canvas.style.cursor = "";
+	}
+	if (isDeleteKeyDown && e.key == deleteKey) {
+		isDeleteKeyDown = false;
+		canvas.style.cursor = "";
+	}
+});
+canvas.addEventListener("mousedown", (e) => {
+	isMouseDown = true;
+	if (!isDragKeyDown) {
 		let cellX = Math.floor((-offsetX + e.offsetX) / cellWidth);
 		let cellY = Math.floor((-offsetY + e.offsetY) / cellWidth);
-		if (life.has(`${cellX},${cellY}`)) {
-			life.delete(`${cellX},${cellY}`);
+		let targetCell = `${cellX},${cellY}`;
+		if (life.has(targetCell)) {
+			life.delete(targetCell);
 		} else {
-			life.add(`${cellX},${cellY}`);
+			life.add(targetCell);
 		}
 	}
-	didDrag = false;
+	if (isDragKeyDown) {
+		canvas.style.cursor = "grabbing";
+	}
+});
+canvas.addEventListener("mouseup", (e) => {
+	isMouseDown = false;
+	if (isDragKeyDown) {
+		canvas.style.cursor = "grab";
+	}
 	render();
 });
 
 canvas.addEventListener("mousemove", (e) => {
 	if (isMouseDown) {
-		offsetX += e.movementX * 1;
-		offsetY += e.movementY * 1;
-		if ((e.movementX**2+e.movementY**2)**0.5 > 5) {
-			didDrag = true;
+		if (isDragKeyDown) {
+			offsetX += e.movementX * 1;
+			offsetY += e.movementY * 1;
+		} else {
+			let cellX = Math.floor((-offsetX + e.offsetX) / cellWidth);
+			let cellY = Math.floor((-offsetY + e.offsetY) / cellWidth);
+			if (isDeleteKeyDown) {
+				life.delete(`${cellX},${cellY}`);
+			} else {
+				life.add(`${cellX},${cellY}`);
+			}
 		}
 	}
 	render();
